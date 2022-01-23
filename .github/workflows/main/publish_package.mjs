e629f6edb2125954df5fe6f21603e45646b9494d
dmail <dmaillard06@gmail.com>
2022-01-23 16:23:18 +0100

    also publish github release during workflow

--- .github/workflows/main/publish_package.mjs
+++ .github/workflows/main/publish_package.mjs
@@ -2,14 +2,20 @@ import { publishPackage } from "@jsenv/workflow/packages/package-publish"
 
 const rootDirectoryUrl = new URL("../../../", import.meta.url)
 
-await ["jsenv-package-publish"].reduce(async (previous, packageName) => {
-  await previous
-  await publishPackage({
-    projectDirectoryUrl: new URL(`./packages/${packageName}`, rootDirectoryUrl),
-    registriesConfig: {
-      "https://registry.npmjs.org": {
-        token: process.env.NPM_TOKEN,
+await ["jsenv-github-release-package", "jsenv-package-publish"].reduce(
+  async (previous, packageName) => {
+    await previous
+    await publishPackage({
+      projectDirectoryUrl: new URL(
+        `./packages/${packageName}`,
+        rootDirectoryUrl,
+      ),
+      registriesConfig: {
+        "https://registry.npmjs.org": {
+          token: process.env.NPM_TOKEN,
+        },
       },
-    },
-  })
-}, Promise.resolve())
+    })
+  },
+  Promise.resolve(),
+)
