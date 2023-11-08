bb8ec0bef47866ee24a49e412b8d110612989618
dmail <dmaillard06@gmail.com>
2023-11-08 09:54:40 +0100

    introduce upgradePackagesVersions

--- packages/monorepo/src/main.js
+++ packages/monorepo/src/main.js
@@ -1,2 +1,3 @@
+export { upgradePackagesVersions } from "./upgrade_packages_versions.js";
 export { syncPackagesVersions } from "./sync_packages_versions.js";
 export { publishPackages } from "./publish_packages.js";
